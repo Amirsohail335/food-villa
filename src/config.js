cb47d76447617552cd911b509373f44c2370dd6a
Amir Sohail <amirsohail335@gmail.com>
2023-03-13 20:42:16 +0530

    committed

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const IMAGE_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
-export const restaurantList  = [
+export const restaurantList = [
   {
     name: "Burger King",
     image:
@@ -9,7 +9,6 @@ export const restaurantList  = [
     cusines: ["Burger", "American"],
     rating: "4.2",
   },
-
 
   {
     type: "restaurant",
